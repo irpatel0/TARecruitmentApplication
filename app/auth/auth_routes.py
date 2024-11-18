c1d4c381736c61424048a4957974a7383452466e
Sai Teja Sunku <128723424+ssunku6@users.noreply.github.com>
2024-11-18 11:37:46 -0500

    created tables for User, Student, Instructor in models.py

--- app/auth/auth_routes.py
+++ app/auth/auth_routes.py
@@ -2,3 +2,7 @@ from flask import render_template, flash, redirect, url_for, request
 from app import db
 from app.auth import auth_blueprint as bp_auth
 import sqlalchemy as sqla
+
+@bp_auth.route('/user/login', methods=['GET', 'POST'])
+def login():
+    return render_template('login.html')
