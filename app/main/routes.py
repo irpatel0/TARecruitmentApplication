d44ce411f73d2c530d2f2da81e8336c1cc432fc0
JDGrier <jake.d.grier@gmail.com>
2024-11-18 19:27:44 -0500

    Filled Course table

--- app/main/routes.py
+++ app/main/routes.py
@@ -4,7 +4,6 @@ from app import db
 from app.main import main_blueprint as bp_main
 from flask_login import current_user, login_required
 from app.main.models import Course, Position
-from app.main.forms import CourseForm, PositionForm
 
 
 @bp_main.route('/', methods=['GET'])
@@ -15,10 +14,11 @@ def index():
 @bp_main.route('/student_index', methods=['GET'])
 @login_required
 def student_index():
+    positions = db.session.query(Position).all()
     if current_user.user_type != 'Student':
         flash('You are not allowed to access Student Page')
         return redirect(url_for('auth.login'))
-    return render_template('student_index.html')
+    return render_template('student_index.html', positions = positions)
 
 @bp_main.route('/instructor_index', methods=['GET'])
 @login_required
