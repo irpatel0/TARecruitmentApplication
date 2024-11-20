b931cf095d0bd1619ba903cdf57d7a041f5b5be1
Ishaan Patel <irpatel@wpi.edu>
2024-11-20 01:10:53 -0500

    Model tests

--- tests/test_models.py
+++ tests/test_models.py
@@ -4,7 +4,7 @@ warnings.filterwarnings("ignore")
 from datetime import datetime, timedelta
 import unittest
 from app import create_app, db
-from app.main.models import User, Student, Instructor
+from app.main.models import User, Student, Instructor, Course, CourseSection, Position
 from config import Config
 
 class TestConfig(Config):
@@ -62,3 +62,44 @@ class TestModels(unittest.TestCase):
         self.assertEqual(student.email, 'test@wpi.edu')
         self.assertEqual(student.wpi_id, '456789012')
         self.assertEqual(student.phone, '9087096751')
+
+    def test_create_course(self):
+        course = Course(number='CS1010', title='Introduction to Programming')
+        db.session.add(course)
+        db.session.commit()
+        self.assertEqual(course.number, 'CS1010')
+        self.assertEqual(course.title, 'Introduction to Programming')
+
+    def test_create_course_section(self):
+        course = Course(number='CS1010', title='Introduction to Programming')
+        db.session.add(course)
+        db.session.commit()
+        instructor = Instructor(username='professor', firstname='John', lastname='Doe', wpi_id='123456789', email='john.doe@wpi.edu', phone='1234567890')
+        db.session.add(instructor)
+        db.session.commit()
+        course_section = CourseSection(course_number=course.id, section='A01', instructor_id=instructor.id, term='2023A')
+        db.session.add(course_section)
+        db.session.commit()
+        self.assertEqual(course_section.course_number, course.id)
+        self.assertEqual(course_section.section, 'A01')
+        self.assertEqual(course_section.instructor_id, instructor.id)
+        self.assertEqual(course_section.term, '2023A')
+
+    def test_create_position(self):
+        course = Course(number='CS1010', title='Introduction to Programming')
+        db.session.add(course)
+        db.session.commit()
+        instructor = Instructor(username='professor', firstname='John', lastname='Doe', wpi_id='123456789', email='john.doe@wpi.edu', phone='1234567890')
+        db.session.add(instructor)
+        db.session.commit()
+        course_section = CourseSection(course_number=course.id, section='A01', instructor_id=instructor.id, term='2023A')
+        db.session.add(course_section)
+        db.session.commit()
+        position = Position(section_id=course_section.id, num_SAs=2, available=True, min_GPA=3.0, min_grade='B')
+        db.session.add(position)
+        db.session.commit()
+        self.assertEqual(position.section_id, course_section.id)
+        self.assertEqual(position.num_SAs, 2)
+        self.assertTrue(position.available)
+        self.assertEqual(position.min_GPA, 3.0)
+        self.assertEqual(position.min_grade, 'B')
