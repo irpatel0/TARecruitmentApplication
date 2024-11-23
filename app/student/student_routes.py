676457f3c041588f7dc1978092b4c61d69009f4a
Sai Teja Sunku <128723424+ssunku6@users.noreply.github.com>
2024-11-23 15:56:25 -0500

    edited javascript to display position information in student index

--- app/student/student_routes.py
+++ app/student/student_routes.py
@@ -2,8 +2,16 @@ from flask import render_template, flash, redirect, url_for, request, jsonify
 import sqlalchemy as sqla
 from app import db
 from app.student import student_blueprint as bp_student
+from flask_login import current_user, login_required
+from app.decorators import role_required
+from app.main.models import Course, Position,  CourseSection
 
-
+@bp_student.route("/positions/<position_id>/details", methods=["GET", "POST"])
+@login_required
+@role_required('Student')
+def view_SA_details(position_id):
+    thePosition = db.session.get(Position, position_id)
+    return jsonify({'num_SAs': thePosition.num_SAs, 'available': thePosition.available, 'min_GPA': thePosition.min_GPA, 'min_grade': thePosition.min_grade})
 
 
 
