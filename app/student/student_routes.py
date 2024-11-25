b0d32ebb3f298d4d40d43381a3b153b6c689689b
asanjay3 <asanjay3@wpi.edu>
2024-11-25 17:12:05 -0500

    Student can see instructor name under SA positions

--- app/student/student_routes.py
+++ app/student/student_routes.py
@@ -4,7 +4,7 @@ from app import db
 from app.student import student_blueprint as bp_student
 from flask_login import current_user, login_required
 from app.decorators import role_required
-from app.main.models import Course, Position,  CourseSection
+from app.main.models import Course, Position, Instructor, CourseSection
 
 @bp_student.route("/positions/<position_id>/details", methods=["GET", "POST"])
 @login_required
@@ -13,8 +13,9 @@ def view_SA_details(position_id):
     thePosition = db.session.get(Position, position_id)
     courseSectionID = thePosition.section_id
     theCourseSection = db.session.get(CourseSection, courseSectionID)
-    instructor = theCourseSection.instructor_id
-    return jsonify({'num_SAs': thePosition.num_SAs, 'available': thePosition.available, 'min_GPA': thePosition.min_GPA, 'min_grade': thePosition.min_grade, 'date_posted': thePosition.timestamp, 'instructor' : instructor})
+    instructor = db.session.get(Instructor, theCourseSection.instructor_id)
+    instructor_name = instructor.firstname + " " + instructor.lastname
+    return jsonify({'num_SAs': thePosition.num_SAs, 'available': thePosition.available, 'min_GPA': thePosition.min_GPA, 'min_grade': thePosition.min_grade, 'date_posted': thePosition.timestamp, 'instructor' : instructor_name})
 
 
 
